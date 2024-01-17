e18f553f8807b9805c6fe15245842c9098f6d815
Sukhveenkaur <sukhveen1405.be21@chitkara.edu.in>
2024-01-17 22:17:22 +0530

    change

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ app.use("/", authController);
 app.use("/", blogController);
 app.use("/", adminController);
 
-
+const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(3334, () => {
+    app.listen(port, () => {
       console.log(`Server started on port 3334`);
     });
   })
